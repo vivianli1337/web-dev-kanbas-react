8c488e0bd6d3574f03661f2cab7e0171858e180b
vivianli1337 <vivianli1337@gmail.com>
2024-09-23 22:54:50 -0400

    first commit

---
+++ src/Labs/index.tsx
@@ -0,0 +1,15 @@
+import Lab1 from "./Lab1";
+import Lab2 from "./Lab2";
+import Lab3 from "./Lab3";
+import Lab4 from "./Lab4";
+
+export default function Labs() {
+    return(
+        <div>
+            <Lab1 />
+            <Lab2 />
+            <Lab3 />
+            <Lab4 />
+        </div>
+    )
+}
