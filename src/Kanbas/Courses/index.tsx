61c2d1ac251b4fc2eba42c526dd33c4f19b1abd9
vivianli1337 <vivianli1337@gmail.com>
2024-10-08 13:10:42 -0400

    a2

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -4,30 +4,33 @@ import Modules from "./Modules";
 import Home from "./Home";
 import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/Editor";
+import { FaAlignJustify } from "react-icons/fa";
+
 
 export default function Courses() {
   return (
     <div id="wd-courses">
-      <h2>Course 1234</h2>
+      <h2 className="text-danger" >Course 1234</h2>
+      <FaAlignJustify className="me-4 fs-4 mb-1" />
       <hr />
-      <table>
-        <tr>
-          <td valign="top">
-            <CoursesNavigation />
-          </td>
-          <td valign="top">
-            <Routes>
-              <Route path="/" element={<Navigate to="Home" />} />
-              <Route path="Home" element={<Home />} />
-              <Route path="Modules" element={<Modules />} />
-              <Route path="Assignments" element={<Assignments />} />
-              <Route path="Assignments/:aid" element={<AssignmentEditor />} />
-              <Route path="People" element={<h2>People</h2>} />
+      <div className="d-flex">
+        <div className="d-none d-md-block">
 
-            </Routes>
-          </td>
-        </tr>
-      </table>
+          <CoursesNavigation />
+        </div>
+        <div className="flex-fill">
+
+          <Routes>
+            <Route path="/" element={<Navigate to="Home" />} />
+            <Route path="Home" element={<Home />} />
+            <Route path="Modules" element={<Modules />} />
+            <Route path="Assignments" element={<Assignments />} />
+            <Route path="Assignments/:aid" element={<AssignmentEditor />} />
+            <Route path="People" element={<h2>People</h2>} />
+
+          </Routes>
+        </div>
+      </div>
     </div>
   );
 }
