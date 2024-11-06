3adc530b45735b3ba7a54cbd9bec98c5e1625bfe
vivianli1337 <vivianli1337@gmail.com>
2024-11-05 21:26:15 -0500

    edits

--- src/Kanbas/Courses/Modules/ModuleEditor.tsx
+++ src/Kanbas/Courses/Modules/ModuleEditor.tsx
@@ -1,5 +1,40 @@
+// export default function ModuleEditor({ dialogTitle, moduleName, setModuleName, addModule }:
+//     { dialogTitle: string; moduleName: string; setModuleName: (name: string) => void; addModule: () => void; }) {
+//     return (
+//         <div id="wd-add-module-dialog" className="modal fade" data-bs-backdrop="static" data-bs-keyboard="false">
+//             <div className="modal-dialog">
+//                 <div className="modal-content">
+//                     <div className="modal-header">
+//                         <h1 className="modal-title fs-5" id="staticBackdropLabel">
+//                             {dialogTitle} </h1>
+//                         <button type="button" className="btn-close" data-bs-dismiss="modal"></button>
+//                     </div>
+//                     <div className="modal-body">
+//                         <input className="form-control" defaultValue={moduleName} placeholder="Module Name"
+//                             onChange={(e) => setModuleName(e.target.value)} />
+//                     </div>
+//                     <div className="modal-footer">
+//                         <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">
+//                             Cancel </button>
+//                         <button onClick={addModule} type="button" data-bs-dismiss="modal" className="btn btn-danger">
+//                             Add Module </button>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
+import { useSelector } from "react-redux";
+
 export default function ModuleEditor({ dialogTitle, moduleName, setModuleName, addModule }:
     { dialogTitle: string; moduleName: string; setModuleName: (name: string) => void; addModule: () => void; }) {
+    const { currentUser } = useSelector((state: any) => state.accountReducer);
+    const isFaculty = currentUser?.role === 'FACULTY';
+
+    if (!isFaculty) {
+        return null;
+    }
+
     return (
         <div id="wd-add-module-dialog" className="modal fade" data-bs-backdrop="static" data-bs-keyboard="false">
             <div className="modal-dialog">
@@ -24,3 +59,4 @@ export default function ModuleEditor({ dialogTitle, moduleName, setModuleName, a
         </div>
     );
 }
+
